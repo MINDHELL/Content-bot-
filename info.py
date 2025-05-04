fd117a77f16b8817c40009d7228fbfb0ba4b6266
MINDHELL <Tgpurpose640@gmail.com>
2025-05-04 18:00:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,3 +1,5 @@
+from os import environ 
+
 API = environ.get("API", "64bdbe8b466b812f48060d082a8a8343aae04988") # shortlink api
 URL = environ.get("URL", "linkcents.com") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/test89p/3") # how to open link 
