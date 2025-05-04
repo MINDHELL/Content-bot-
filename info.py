3745463480e0f3df90e4bf594c3d5f38c682b4e4
MINDHELL <Tgpurpose640@gmail.com>
2025-05-04 21:39:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+API = environ.get("API", "64bdbe8b466b812f48060d082a8a8343aae04988"") # shortlink api
+URL = environ.get("URL", "linkcents.com") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/test89p/3") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "Rundumbot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
