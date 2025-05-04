dd4a29e7c39caf11fc0a36ecc0e311f76d68b148
MINDHELL <Tgpurpose640@gmail.com>
2025-05-04 17:47:58 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,5 @@
+API = environ.get("API", "64bdbe8b466b812f48060d082a8a8343aae04988") # shortlink api
+URL = environ.get("URL", "linkcents.com") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/test89p/3") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "Rundumbot") # bot username without @
+VERIFY = environ.get("VERIFY", "True")
